7cdd42341753656e6cc939fa41e7eb8687943e76
dsaladbar617 <dsalazar219@gmail.com>
2022-07-11 14:52:49 -0500

    so am I - Dayan

--- server/middleware/decodeToken.js
+++ server/middleware/decodeToken.js
@@ -1,22 +1,6 @@
 import admin from "../config/firebase_config.js"
 import {getAuth} from "firebase-admin/auth"
 
-// class Middleware {
-//   async decodeToken(req, res, next) {
-//     const token = req.headers.authorization.split(" ")[1];
-//     try {
-//       const decodeValue = await admin.auth().verifyIdToken(token);
-//       if (decodeValue) {
-//         req.user = decodeValue;
-//         return next();
-//       }
-//       return res.json({ message: "Un authorize" });
-//     } catch (e) {
-//       return res.json({ message: "Internal Error" });
-//     }
-//   }
-// }
-
 const decodedToken = (req, res, next) => {
   const auth = req.headers.authorization
   
